dc832fb0706c13f8b45c2e4760d747a065992cf6
Marek Grzyb <marek@grzyb.dev>
2025-11-03 19:48:37 +0100

    Defined typer app for Metal Gear 1 tools

--- src/mgtools/__init__.py
+++ src/mgtools/__init__.py
@@ -1,6 +1,12 @@
 import typer
 
+from mgtools import mg1
+
 app = typer.Typer(help="Tools for modding PC ports of Metal Gear / Metal Gear 2")
+
+app.add_typer(
+    typer_instance=mg1.app, name="mg1", help="Tools for modding PC ports of Metal Gear"
+)
 
 
 if __name__ == "__main__":
---
+++ src/mgtools/mg1/__init__.py
@@ -0,0 +1,6 @@
+import typer
+
+app = typer.Typer(help="Tools for modding PC ports of Metal Gear")
+
+if __name__ == "__main__":
+    app()
