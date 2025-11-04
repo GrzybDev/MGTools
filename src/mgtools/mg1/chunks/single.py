8cc9fb0b398dafb4368be164cfa03da28725f4ae
Marek Grzyb <marek@grzyb.dev>
2025-11-04 10:56:14 +0100

    Added data property in SingleChunk

--- src/mgtools/mg1/chunks/single.py
+++ src/mgtools/mg1/chunks/single.py
@@ -1,4 +1,11 @@
+from io import BytesIO
+
+
 class SingleChunk:
+
+    @property
+    def data(self) -> BytesIO:
+        return BytesIO(self.__data)
 
     def __init__(self, data: bytes):
         self.__data = data
